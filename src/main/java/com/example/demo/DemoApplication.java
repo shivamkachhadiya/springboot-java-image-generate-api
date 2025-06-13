d68629e78793aaa9fc4bca57455bcfc411ac352a
shivamkachhadiya <shivamkachhadiya68@gmail.com>
2025-06-13 18:56:57 +0530

    this is java dog image generate via api.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("god is grat....");
+		System.out.println("hello this is main file");
 	}
 
 }
